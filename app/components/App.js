1b8a97cf56071a584f09b0dd39d8c7bf6c49a409
Muralimanohar Chepuri <murali.chepuri@gmail.com>
2025-04-30 18:20:14 -0700

    Router fixes

--- app/components/App.js
+++ app/components/App.js
@@ -9,7 +9,7 @@ export  const App = () =>{
         return(
         <div className="app"> 
                 {/*Change me*/}
-                <Router>
+                <Router basename="/murali-portfolio">
                         <Routes>
                                 <Route path="/" exact element={<Main/>} />
                         </Routes>
